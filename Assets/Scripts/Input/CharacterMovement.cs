c2249c7b6a25b2339e36597d3c0d74617d6426db
Christian Collings <44ccollings@gmail.com>
2022-06-21 15:13:53 +0100

    Made camera and movement independent, added sprint

--- Assets/Scripts/Input/CharacterMovement.cs
+++ Assets/Scripts/Input/CharacterMovement.cs
@@ -9,13 +9,16 @@ public class CharacterMovement : MonoBehaviour
     [SerializeField]
     private float gravityValue = -9.81f;
 
+    private float speed;
+
     private CharacterController controller;
     private PlayerInput playerInput;
     private Vector3 playerVelocity;
     private bool groundedPlayer;
 
-    public InputAction moveAction;
-    public InputAction lookAction;
+    private InputAction moveAction;
+    private InputAction lookAction;
+    private InputAction sprintAction;
 
     private void Start()
     {
@@ -23,9 +26,15 @@ public class CharacterMovement : MonoBehaviour
         playerInput = GetComponent<PlayerInput>();
         moveAction = playerInput.actions["Movement"];
         lookAction = playerInput.actions["Look"];
+        sprintAction = playerInput.actions["Sprint"];
     }
 
     void Update()
+    {
+        Movement();
+    }
+
+    void Movement()
     {
         groundedPlayer = controller.isGrounded;
         if (groundedPlayer && playerVelocity.y < 0)
@@ -33,9 +42,24 @@ public class CharacterMovement : MonoBehaviour
             playerVelocity.y = 0f;
         }
 
+        if (moveAction.ReadValue<Vector2>() == Vector2.zero)
+        {
+            
+        }
+        else if (sprintAction.ReadValue<float>() > .1f)
+        {
+            speed = playerSpeed * 4;
+        }
+        else
+        {
+            speed = playerSpeed;
+        }
+
         Vector2 inputValues = moveAction.ReadValue<Vector2>();
         Vector3 move = new Vector3(inputValues.x, 0, inputValues.y);
-        controller.Move(move * Time.deltaTime * playerSpeed);
+        move = Camera.main.transform.TransformDirection(move);
+        move.y = 0.0f;
+        controller.Move(move * Time.deltaTime * speed);
 
         if (move != Vector3.zero)
         {
